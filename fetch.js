8c6add4719c6fe34cbd0201f3e3d1ab4eb4d9276
Matilda Brunemalm <>
2024-02-27 11:22:06 +0100

    Add .catch()

--- fetch.js
+++ fetch.js
@@ -1,6 +1,6 @@
 //https://www.dictionaryapi.com/api/v3/references/thesaurus/json/umpire?key=your-api-key
 
-const BASE_URL = "https://www.dictionaryapi.com/api/v3/references/thesaurus/json/"
+const BASE_URL = "htt://www.dictionaryapi.com/api/v3/references/thesaurus/json/"
 const API_KEY = "1b5c3473-b930-4d32-a959-4b6be1c72f42" //query param
 const word = "conundrum" //path param
 
@@ -10,6 +10,7 @@ const wordTitle = document.getElementById("word")
 const type = document.getElementById("type")
 const explanation = document.getElementById("explanation")
 const synonymsList = document.getElementById("syn")
+const errorDiv = document.getElementById("error")
 
 const updateHTML = (data) => {
   const wordChoice = data[0].hwi.hw
@@ -32,6 +33,10 @@ const fetchWord = () => {
   fetch(URL)
     .then(response => response.json())
     .then(data => updateHTML(data))
+    .catch(error => {
+      console.log(error)
+      errorDiv.innerText = "Something went wrong"
+    })
 }
 
 fetchWord()
